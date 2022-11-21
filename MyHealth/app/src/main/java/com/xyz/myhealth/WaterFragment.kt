c08798458ba26d46ff7e9dcaeb39bb0cbc7d87d2
Ad1tya-007 <adityakul0314@gmail.com>
2022-11-20 23:53:02 -0800

    added basic frontend
    Signed-off-by: Ad1tya-007 <adityakul0314@gmail.com>

---
+++ MyHealth/app/src/main/java/com/xyz/myhealth/WaterFragment.kt
@@ -0,0 +1,14 @@
+package com.xyz.myhealth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class WaterFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_water, container, false)
+    }
+}
